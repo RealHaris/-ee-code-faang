1e2aba58db1081f3f522f7f98a773f5e8f7c4e9f
Haris <haris.khan@kistpay.com>
2025-10-25 03:39:00 +0500

    comment

--- sliding-window/longest-substring-without-repeating-duplicates.js
+++ sliding-window/longest-substring-without-repeating-duplicates.js
@@ -1,34 +1,23 @@
+// THE BEST PART I HAVE DONE FULL LOGIC EXCEPT J CONDITION
+// WORST PART : I COULDNT EVEN DO IT ON MY OWN AND ITS O(N2) BECAUSE OF SLICE
+
 /**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  if (s.length === 1) {
-    return 1;
-  }
+  if (s.length <= 1) return s.length;
 
-  let count = 1,
-    j = 0,
-    max = 0;
+  let j = 0,
+    max = 1;
+
   for (let i = 0; i < s.length - 1; i++) {
-    console.log(
-      "s",
-      s.slice(j, i + 1).includes(s[i + 1]),
-      s.slice(j, i + 1),
-      s[i + 1],
-    );
-    c;
-
-    if (s.slice(j, i + 1).includes(s[i + 1])) {
-      // count--;
-      j = i;
-    } else {
-      count++;
+    let character = s.slice(j, i + 1);
+    if (character.includes(s[i + 1])) {
+      // move j to one after the last occurrence of s[i + 1]
+      j = j + character.indexOf(s[i + 1]) + 1;
     }
-
-    if (max <= count) {
-      max = count;
-    }
+    max = Math.max(max, i + 2 - j);
   }
 
   return max;
